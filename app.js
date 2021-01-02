05ad5f0efa6a274469b1c8746c7aef57a7a15a23
kidaqrus <adikwusule@gmail.com>
2021-01-02 17:57:11 +0000

    make it responsive

--- app.js
+++ app.js
@@ -1,17 +1,12 @@
 const express = require('express')
-const mongoose = require('mongoose')
 const methodOverride = require('method-override')
 const app = express()
-var compression = require('compression');
-const path = require('path');
-var helmet = require('helmet');
 
 
 app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
 app.use(methodOverride('_method'))
-app.use(compression());
-app.use(helmet());
+
 
 app.get('/',  (req, res) => {
   
@@ -19,10 +14,6 @@ app.get('/',  (req, res) => {
   return res.render('index')
 })
 
-app.get('/about', async (req, res) => {
-  const about_author = await aboutBlogger.find().sort({ date_time_track: 'desc' })
-  return res.render('about_views/about', { about_author: about_author })
-})
 // for linking css/other static assets
 app.use(express.static(__dirname + '/public'))
 
